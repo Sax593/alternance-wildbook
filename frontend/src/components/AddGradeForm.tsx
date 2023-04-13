5a90100647b51bb5bcc466ffcad87d574da17088
Priscillia <selvapriscillia@yahoo.fr>
2023-04-13 09:55:45 +0200

    fix bugs

--- frontend/src/components/AddGradeForm.tsx
+++ frontend/src/components/AddGradeForm.tsx
@@ -1,58 +1,62 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { SkillApi } from "../App";
+import { WilderProps } from "./Wilder";
 
 export default function AddGradeForm() {
-  const [wilder, setWilder] = useState<string>("");
-  const [skill, setSkill] = useState<string>("");
-  const [skills, setSkills] = useState<SkillApi[]>([]);
-  const [grade, setGrade] = useState(0);
+  const [wilderId, setWilderId] = useState("");
+  const [skillId, setSkillId] = useState("");
+  const [grade, setGrade] = useState("");
+  const [wilders, setWilders] = useState<WilderProps[]>([]);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/api/skill").then((data: any) => {
-      setSkills(data.data);
+    axios.get("http://localhost:5000/api/wilder").then((data: any) => {
+      setWilders(data.data);
+      console.log(setWilders);
     });
   }, []);
 
   return (
-    <div>
-      <form
-        className="form"
-        onSubmit={() => {
-          axios.post("http://localhost5000/api/grade", {
-            wilder: wilder,
-            skill: skill,
-            grade: grade,
-          });
-        }}>
-        <label>Select a Wilder : </label>
-        <select name="wilders" onChange={(e) => setWilder(e.target.value)}>
-          <option value="">--Select a wilder--</option>
-          {wilder.map((wilder, key) => (
-            <option key={key} value={wilder.id}>
-              {wilder.name}
-            </option>
-          ))}
-        </select>
-        <label>Select a Skill : </label>
-        <select name="skills" onChange={(e) => setSkill(e.target.value)}>
-          <option value="">--Select a skill--</option>
-          {skills.map((skill, key) => (
-            <option key={key} value={skill.id}>
-              {skill.name}
-            </option>
-          ))}
-        </select>
-        <label>Add a grade : </label>
-        <input
-          type="number"
-          min="0"
-          max="10"
-          value={grade}
-          onChange={(e) => setGrade(parseInt(e.target.value))}
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        axios.post("http://localhost:5000/api/grade", {
+          wilderId: wilderId,
+          skillId: skillId,
+          grade: grade,
+        });
+      }}>
+      wilderid
+      <select
+        onChange={(e) => {
+          setWilderId(e.target.value);
+        }}
+        name="wilders"
+        id="wilderselect">
+        <option value="">--Please choose an option--</option>
+        {wilders.map((wilder) => (
+          <option key={wilder.id} value={wilder.id}>
+            {wilder.name}
+          </option>
+        ))}
+      </select>
+      <br></br>
+      skillid
+      <input
+        value={skillId}
+        onChange={(e) => {
+          setSkillId(e.target.value);
+        }}
+      />
+      <br></br>
+      grade
+      <input
+        value={grade}
+        onChange={(e) => {
+          setGrade(e.target.value);
+        }}
+      />
+      <br></br>
+      <button>Submit</button>
+    </form>
   );
 }
